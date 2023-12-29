4f90bd10df7a5e9d5b0d5ee1a07df4a99d2ad863
Yorlock <dominikluk2@gmai.com>
2023-12-29 14:18:33 +0100

    Init project

---
+++ src/main/java/zamowienie_pizzy/ZamowieniePizzy.java
@@ -0,0 +1,9 @@
+package zamowienie_pizzy;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Zamowienie Pizzy")
+public class ZamowieniePizzy extends ServletProcessApplication{
+
+}
